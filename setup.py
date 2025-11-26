e65f30f1f4e5664f783c616cdfe046967b0cdbc8
Kshiti <kshitismita.panda@gmail.com>
2025-11-26 16:42:59 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_bot",
+    version="0.1.0",
+    author="Kshitismita Panda",
+    author_email="kshitismitapanda@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
